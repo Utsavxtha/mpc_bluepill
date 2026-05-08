557d3eedce43932886a54bd07692a20905eb9201
Ujwol Acharya <ujwolacharya4@gmail.com>
2026-05-08 12:30:50 +0545

    added the kinematics and PID feature
    Modified-by: Ujwol Acharya <ujwolacharya4@gmail.com>

--- Core/Src/car.cpp
+++ Core/Src/car.cpp
@@ -30,11 +30,37 @@ void Car::run()
   set_twist(twist);
   // update();
   // left_motor.set_speed(0.2f);
-  printf("hello\n");
+  // printf("hello\n");
 }
 
 void Car::set_twist(const Twist &_twist)
 {
+  const Twist tw = _twist;
+  float vel = tw.vx;
+  float w = tw.w;
+  // now use them to generate velocity for wheels
+
+  // Quick math was done for inverse kinematics
+  // could be wrong, have to test it first
+  float omega_right = (vel / (WHEEL_DIAMETER / 2));
+  omega_right += (w * (BASE_DIAMETER / 2) / (WHEEL_DIAMETER));
+
+  float omega_left = (vel / (WHEEL_DIAMETER / 2));
+  omega_left -= (w * (BASE_DIAMETER / 2) / (WHEEL_DIAMETER));
+
+  // PID work
+  this->right_pid.setpoint = omega_right;
+  this->right_pid.input = right_encoder.get_omega();
+  this->right_pid.compute();
+  float right_pwm = this->right_pid.output;
+  this->right_motor.set_speed(right_pwm);
+
+
+  this->left_pid.setpoint = omega_left;
+  this->left_pid.input = left_encoder.get_omega();
+  this->left_pid.compute();
+  float left_pwm = this->left_pid.output;
+  this->left_motor.set_speed(left_pwm);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *uart)
